21244069d89f933bb85f7221cc87d2cc6e43cee6
shayanahmad1999 <shayanahmad235@gmail.com>
2023-12-19 16:32:30 +0500

    seeder for request is created

--- database/seeders/RequestsSeeder.php
+++ database/seeders/RequestsSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RequestsSeeder extends Seeder
 {
@@ -14,6 +16,13 @@ class RequestsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        for ($i = 1; $i <= 20; $i++) {
+            DB::table('recieved_requests')->insert([
+                'user1_id' => User::inRandomOrder()->value('id'),
+                'user2_id' => User::inRandomOrder()->value('id'),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
 }
