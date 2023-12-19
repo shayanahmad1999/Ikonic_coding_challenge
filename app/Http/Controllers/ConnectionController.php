c21a4fd4b9ed8aff5ea1dc844ae112c8a5d851ce
shayanahmad1999 <shayanahmad235@gmail.com>
2023-12-19 16:29:18 +0500

    connection work done without loadmore

--- app/Http/Controllers/ConnectionController.php
+++ app/Http/Controllers/ConnectionController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Connection;
+use App\Models\RecievedRequest;
 use Illuminate\Http\Request;
 
 class ConnectionController extends Controller
 {
-    //
+
+    public function index(Request $request)
+    {
+        // Logic to fetch and return sent requests data
+        $currentUser = auth()->user();
+        $connections = Connection::where('user1_id', $currentUser->id)->get();
+
+        return view('connections.get_connections', compact('connections'));
+    }
+
+    public function create(Request $request, $userId)
+    {
+        // Logic to send a connection request and insert into SendRequest table
+        $currentUser = auth()->user();
+
+        // Check if the connection request already exists
+        $existingRequest = Connection::where('user1_id', $currentUser->id)
+            ->where('user2_id', $userId)
+            ->first();
+
+        if (!$existingRequest) {
+            // Create a new connection request
+            Connection::create([
+                'user1_id' => $currentUser->id,
+                'user2_id' => $userId,
+            ]);
+
+            RecievedRequest::where('user1_id', $userId)
+                ->where('user2_id', $currentUser->id)
+                ->delete();
+
+            return response()->json(['message' => 'Connection create successfully.']);
+        } else {
+            return response()->json(['message' => 'Connection already sent.']);
+        }
+    }
+    public function destroy($userId)
+    {
+        $currentUser = auth()->user();
+        $destroy = Connection::where('user1_id', $currentUser->id)
+            ->where('user2_id', $userId)
+            ->delete();
+
+        if ($destroy !== 0) {
+            return response()->json(['success' => true]);
+        } else {
+            return response()->json(['success' => false, 'message' => 'Record not found.']);
+        }
+    }
 }
--- resources/views/components/connection.blade.php
+++ resources/views/components/connection.blade.php
@@ -1,32 +1,82 @@
+@php
+    use App\Models\ConnectionInCommom;
+@endphp
 <div class="my-2 shadow text-white bg-dark p-1" id="">
-  <div class="d-flex justify-content-between">
-    <table class="ms-1">
-      <td class="align-middle">Name</td>
-      <td class="align-middle"> - </td>
-      <td class="align-middle">Email</td>
-      <td class="align-middle">
-    </table>
-    <div>
-      <button style="width: 220px" id="get_connections_in_common_" class="btn btn-primary" type="button"
-        data-bs-toggle="collapse" data-bs-target="#collapse_" aria-expanded="false" aria-controls="collapseExample">
-        Connections in common ()
-      </button>
-      <button id="create_request_btn_" class="btn btn-danger me-1">Remove Connection</button>
-    </div>
+    <div class="d-flex justify-content-between">
+        <table class="ms-1">
+            <td class="align-middle">Name: {{ $user->name }}</td>
+            <td class="align-middle"> - </td>
+            <td class="align-middle">Email: {{ $user->email }}</td>
+            <td class="align-middle">
+        </table>
+        <div>
+            @php
+                $connectionCommon = ConnectionInCommom::where('user1_id', $user->id)->count();
+                $connectionCommons = ConnectionInCommom::where('user1_id', $user->id)->get();
+            @endphp
+            @if ($connectionCommon)
+                <button style="width: 220px" id="get_connections_in_common_" class="btn btn-primary" type="button"
+                    data-bs-toggle="collapse" data-bs-target="#collapse_{{ $user->id }}" aria-expanded="false"
+                    aria-controls="collapseExample">
+                    Connections in common ({{ $connectionCommon }})
+                </button>
+            @else
+                <button style="width: 220px" id="get_connections_in_common_" class="btn btn-primary d-none"
+                    type="button" data-bs-toggle="collapse" data-bs-target="#collapse_{{ $user->id }}"
+                    aria-expanded="false" aria-controls="collapseExample">
+                    Connections in common ({{ $connectionCommon }})
+                </button>
+            @endif
+            <button id="create_request_btn_" class="btn btn-danger me-1 remove"
+                data-user-id="{{ $user->id }}">Remove Connection</button>
+        </div>
 
-  </div>
-  <div class="collapse" id="collapse_">
+    </div>
+    <div class="collapse" id="collapse_{{ $user->id }}">
 
-    <div id="content_" class="p-2">
-      {{-- Display data here --}}
-      <x-connection_in_common />
-    </div>
-    <div id="connections_in_common_skeletons_">
-      {{-- Paste the loading skeletons here via Jquery before the ajax to get the connections in common --}}
-    </div>
-    <div class="d-flex justify-content-center w-100 py-2">
+        <div id="content_" class="p-2">
+            {{-- Display data here --}}
+            @foreach ($connectionCommons as $data)
+                <x-connection_in_common :user="$data->connectedUser" />
+            @endforeach
+        </div>
+        <div id="connections_in_common_skeletons_">
+            {{-- Paste the loading skeletons here via Jquery before the ajax to get the connections in common --}}
+        </div>
+        {{-- <div class="d-flex justify-content-center w-100 py-2">
       <button class="btn btn-sm btn-primary" id="load_more_connections_in_common_">Load
         more</button>
+    </div> --}}
     </div>
-  </div>
 </div>
+
+<script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('.remove').on('click', function() {
+            var userId = $(this).data('user-id');
+            removeConnectionRequest(userId);
+        });
+
+        function removeConnectionRequest(userId) {
+            $.ajax({
+                url: '/remove-connection/' + userId,
+                type: 'get',
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                },
+                success: function(response) {
+                    // Handle success, e.g., show a message or update the UI
+                    console.log(response);
+                    if (response.success) {
+                        // alert('Connection request sent successfully.');
+                        // Optionally, you can hide the connected user's card here
+                        $(`.remove[data-user-id="${userId}"]`).closest('.my-2').hide();
+                    } else {
+                        // alert(response.message);
+                    }
+                },
+            });
+        }
+    });
+</script>
